18196e7bd5d6d9ba2cb1b7f7e75a2ede7df0236f
平野優二 <jamjam5169@gmail.com>
2021-10-03 18:39:36 +0900

    no message

--- lime/resources/js/menu.js
+++ lime/resources/js/menu.js
@@ -1,91 +1,10 @@
 const { default: axios } = require("axios");
 
 $(function(){
-
-///メニュー１（２7個） 
-$('.egg').on('click', function(){
-   $(this).toggleClass('isActive');
-   })
-     $('.milk').on('click', function(){
-         $(this).toggleClass('isActive');
-         })
-         $('.wheat').on('click', function(){
-            $(this).toggleClass('isActive');
-            })
-            $('.buckwheat').on('click', function(){
-                $(this).toggleClass('isActive');
-                })
-                $('.peanut').on('click', function(){
-                    $(this).toggleClass('isActive');
-                    })
-                    $('.shrimp').on('click', function(){
-                        $(this).toggleClass('isActive');
-                        })
-                        $('.crab').on('click', function(){
-                            $(this).toggleClass('isActive');
-                            })
-                            $('.abalone').on('click', function(){
-                                $(this).toggleClass('isActive');
-                                })
-                                $('.cuttlefish').on('click', function(){
-                                    $(this).toggleClass('isActive');
-                                    })
-                                    $('.salmonrow').on('click', function(){
-                                        $(this).toggleClass('isActive');
-                                        })
-                                        $('.orange').on('click', function(){
-                                            $(this).toggleClass('isActive');
-                                            })
-                                            $('.cashewnut').on('click', function(){
-                                                $(this).toggleClass('isActive');
-                                                })
-                                                $('.kiwi').on('click', function(){
-                                                    $(this).toggleClass('isActive');
-                                                    })
-                                                    $('.beaf').on('click', function(){
-                                                        $(this).toggleClass('isActive');
-                                                        })
-                                                        $('.walnut').on('click', function(){
-                                                            $(this).toggleClass('isActive');
-                                                            })
-                                                            $('.sesame').on('click', function(){
-                                                                $(this).toggleClass('isActive');
-                                                                })
-                                                                $('.salmon').on('click', function(){
-                                                                    $(this).toggleClass('isActive');
-                                                                    })
-                                                                    $('.mackerel').on('click', function(){
-                                                                        $(this).toggleClass('isActive');
-                                                                        })
-                                                                        $('.soybean').on('click', function(){
-                                                                            $(this).toggleClass('isActive');
-                                                                            })
-                                                                            $('.chicken').on('click', function(){
-                                                                                $(this).toggleClass('isActive');
-                                                                                })
-                                                                                $('.banana').on('click', function(){
-                                                                                    $(this).toggleClass('isActive');
-                                                                                    })
-                                                                                    $('.pork').on('click', function(){
-                                                                                        $(this).toggleClass('isActive');
-                                                                                        })
-                                                                                        $('.mashroom').on('click', function(){
-                                                                                            $(this).toggleClass('isActive');
-                                                                                            })
-                                                                                            $('.peach').on('click', function(){
-                                                                                                $(this).toggleClass('isActive');
-                                                                                                })
-                                                                                                $('.yam').on('click', function(){
-                                                                                                    $(this).toggleClass('isActive');
-                                                                                                    })
-                                                                                                    $('.apple').on('click', function(){
-                                                                                                        $(this).toggleClass('isActive');
-                                                                                                        })
-                                                                                                        $('.gelatin').on('click', function(){
-                                                                                                            $(this).toggleClass('isActive');
-                                                                                                            })
-                                                                                                            $('.almond').on('click', function(){
-                                                                                                                $(this).toggleClass('isActive');
-                                                                                                                })
-                                                                                                               
+    $('.mask').on('click', function(){
+        var obj = $(this).parent('div');
+        var checkbox = $(obj).find('input');
+        $(checkbox).trigger("click");
+    });
+                                                                
  });                                                                                                              
--- lime/resources/views/menu.blade.php
+++ lime/resources/views/menu.blade.php
@@ -20,7 +20,7 @@
 　　</div>
 　　
 　　<div id="menu">
-　　    <h1>〜menu一覧〜</h1>
+　　    <h1>〜menu〜</h1>
 <select id="menuselect">  <!-- メニューの種類　※利用者入力項目　　 -->
        <option>メイン</option>
        <option>ドリンク</option>
@@ -47,177 +47,202 @@
 
 <!-- アレルギーアイコン -->
 　<div id="allergies">
-<div class="egg">
-<img src="images/egg.gif" id="ico" alt="卵"/>
-    <div class="mask">
-		<div class="caption">たまご</div>
-	</div> 
-</div>
 
-<div class="milk">
-<img src="images/milk.gif" id="ico" alt="牛乳"/>
-    <div class="mask">
-		<div class="caption">牛乳</div>
-	</div> 
-</div>
+<div>
+      <input type="checkbox" name="" class="allergies_check egg" />
+      <div class="mask">
+		    <div class="caption">たまご</div>
+	    </div>
+    </div>
 
-<div class="wheat">
-<img src="images/wheat.gif" id="ico" alt="小麦"/>
-    <div class="mask">
-		<div class="caption">小麦</div>
-	</div> 
-</div>
-<div class="buckwheat">
-<img src="images/buckwheat.gif" id="ico" alt="そば"/>
-    <div class="mask">
-		<div class="caption">そば</div>
-	</div> 
-</div>
-<div class="peanut">
-<img src="images/peanut.gif" id="ico" alt="落花生"/>
-    <div class="mask">
-		<div class="caption">落花生</div>
-	</div> 
-</div>
-<div class="shrimp">
-<img src="images/shrimp.gif" id="ico" alt="エビ"/>
-    <div class="mask">
-		<div class="caption">エビ</div>
-	</div> 
-</div>
-<div class="crab">
-<img src="images/crab.gif" id="ico" alt="カニ"/>
-    <div class="mask">
-		<div class="caption">カニ</div>
-	</div> 
-</div>
-<div class="abalone">
-<img src="images/abalone.gif" id="ico" alt="アワビ"/>
-    <div class="mask">
-		<div class="caption">アワビ</div>
-	</div> 
-</div>
-<div class="cuttlefish">
-<img src="images/cuttlefish.gif" id="ico" alt="イカ"/>
-    <div class="mask">
-		<div class="caption">イカ</div>
-	</div> 
-</div>
-<div class="salmonrow">
-<img src="images/salmonrow.gif" id="ico" alt="イクラ"/>
-    <div class="mask">
-		<div class="caption">イクラ</div>
-	</div> 
-</div>
-<div class="orange">
-<img src="images/orange.gif" id="ico" alt="みかん"/>
-    <div class="mask">
-		<div class="caption">みかん</div>
-	</div> 
-</div>
-<div class="cashewnut">
-<img src="images/cashewnut.gif" id="ico" alt="カシューナッツ"/>
-    <div class="mask">
-		<div class="caption">カシューナッツ</div>
-	</div> 
-</div>
-<div class="kiwi">
-<img src="images/kiwi.gif" id="ico" alt="キウイ"/>
-    <div class="mask">
-		<div class="caption">キウイ</div>
-	</div> 
-</div>
-<div class="beaf">
-<img src="images/beaf.gif" id="ico" alt="牛"/>
-    <div class="mask">
-		<div class="caption">牛</div>
-	</div> 
-</div>
-<div class="walnut">
-<img src="images/walnut.gif" id="ico" alt="くるみ"/>
-    <div class="mask">
-		<div class="caption">くるみ</div>
-	</div> 
-</div>
-<div class="sesame">
-<img src="images/sesame.gif" id="ico" alt="ごま"/>
-    <div class="mask">
-		<div class="caption">ごま</div>
-	</div> 
-</div>
-<div class="salmon">
-<img src="images/salmon.gif" id="ico" alt="鮭"/>
-    <div class="mask">
-		<div class="caption">さけ</div>
-	</div> 
-</div>
-<div class="mackerel">
-<img src="images/mackerel.gif" id="ico" alt="さば"/>
-    <div class="mask">
-		<div class="caption">さば</div>
-	</div> 
-</div>
-<div class="soybean">
-<img src="images/soybean.gif" id="ico" alt="大豆"/>
-    <div class="mask">
-		<div class="caption">大豆</div>
-	</div> 
-</div>
-<div class="chicken">
-<img src="images/chicken.gif" id="ico" alt="鶏"/>
-    <div class="mask">
-		<div class="caption">にわとり</div>
-	</div> 
-</div>
-<div class="banana">
-<img src="images/banana.gif" id="ico" alt="バナナ"/>
-    <div class="mask">
-		<div class="caption">バナナ</div>
-	</div> 
-</div>
-<div class="pork">
-<img src="images/pork.gif" id="ico" alt="豚"/>
-    <div class="mask">
-		<div class="caption">ぶた</div>
-	</div> 
-</div>
-<div class="mashroom">
-<img src="images/mashroom.gif" id="ico" alt="松茸"/>
-    <div class="mask">
-		<div class="caption">きのこ</div>
-	</div> 
-</div>
-<div class="peach">
-<img src="images/peach.gif" id="ico" alt="もも"/>
-    <div class="mask">
-		<div class="caption">もも</div>
-	</div> 
-</div>
-<div class="yam">
-<img src="images/yam.gif" id="ico" alt="長芋"/>
-    <div class="mask">
-		<div class="caption">長芋</div>
-	</div> 
-</div>
-<div class="apple">
-<img src="images/apple.gif" id="ico" alt="りんご"/>
-    <div class="mask">
-		<div class="caption">りんご</div>
-	</div> 
-</div>
-<div class="gelatin">
-<img src="images/gelatin.gif" id="ico" alt="ゼラチン"/>
-    <div class="mask">
-		<div class="caption">ゼラチン</div>
-	</div> 
-</div>
-<div class="almond">
-<img src="images/almond.gif" id="ico" alt="アーモンド"/>
-    <div class="mask">
-		<div class="caption">アーモンド</div>
-	</div> 
-</div>
-</div>
+    <div>
+      <input type="checkbox" name="" class="allergies_check milk" />
+      <div class="mask">
+		    <div class="caption">牛乳</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check wheat" />
+      <div class="mask">
+		    <div class="caption">小麦</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check buckwheat" />
+      <div class="mask">
+		    <div class="caption">そば</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check peanut" />
+      <div class="mask">
+		    <div class="caption">落花生</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check shrimp" />
+      <div class="mask">
+		    <div class="caption">エビ</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check crab" />
+      <div class="mask">
+		    <div class="caption">カニ</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check abalone" />
+      <div class="mask">
+		    <div class="caption">アワビ</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check cuttlefish" />
+      <div class="mask">
+		    <div class="caption">イカ</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check salmonrow" />
+      <div class="mask">
+		    <div class="caption">イクラ</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check orange" />
+      <div class="mask">
+		    <div class="caption">みかん</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check cashewnut" />
+      <div class="mask">
+		    <div class="caption">カシューナッツ</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check kiwi" />
+      <div class="mask">
+		    <div class="caption">キウイ</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check beaf" />
+      <div class="mask">
+		    <div class="caption">牛</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check walnut" />
+      <div class="mask">
+		    <div class="caption">くるみ</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check sesame" />
+      <div class="mask">
+		    <div class="caption">ごま</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check salmon" />
+      <div class="mask">
+		    <div class="caption">さけ</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check mackerel" />
+      <div class="mask">
+		    <div class="caption">さば</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check soybean" />
+      <div class="mask">
+		    <div class="caption">大豆</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check chicken" />
+      <div class="mask">
+		    <div class="caption">にわとり</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check banana" />
+      <div class="mask">
+		    <div class="caption">バナナ</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check pork" />
+      <div class="mask">
+		    <div class="caption">ぶた</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check mashroom" />
+      <div class="mask">
+		    <div class="caption">きのこ</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check peach" />
+      <div class="mask">
+		    <div class="caption">もも</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check yam" />
+      <div class="mask">
+		    <div class="caption">長芋</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check apple" />
+      <div class="mask">
+		    <div class="caption">りんご</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check gelatin" />
+      <div class="mask">
+		    <div class="caption">ゼラチン</div>
+	    </div>
+    </div>
+
+    <div>
+      <input type="checkbox" name="" class="allergies_check almond" />
+      <div class="mask">
+		    <div class="caption">アーモンド</div>
+	    </div>
+    </div>
     
 <!-- カロリー記載　※利用者記載　 -->
 　　<div id="kcal">
